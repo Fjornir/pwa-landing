50272a29897fdd66c7554d069717428cd2087f99
kkid404 <vizzy1488@gmail.com>
2024-05-13 19:54:11 +0500

    доработал редирект. пока есть проблемка с тем, что если пва удалить localstorage не обновляется

--- src/registerSW.ts
+++ src/registerSW.ts
@@ -3,6 +3,7 @@ export const registerServiceWorker = async () => {
       try {
         const registration = await navigator.serviceWorker.register('/sw.js', { scope: '/' });
         console.log('Service Worker зарегистрирован:', registration);
+        return registration
       } catch (error) {
         console.error('Ошибка при регистрации Service Worker:', error);
       }
