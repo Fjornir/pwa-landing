043c5de03880bb4fc0e66b360d18839cc2eb799a
Nikita <50362451+Fjornir@users.noreply.github.com>
2024-05-14 13:40:37 +0300

    Закончена верстка блока с комментариями

--- src/components/Reviews.tsx
+++ src/components/Reviews.tsx
@@ -12,6 +12,7 @@ export default function Reviews({ amount }: ReviewsDataType) {
     score: "5",
     scoreDate: "March 6, 2024",
     review: "I've been playing my favorite slot machine for a week",
+    helpful: 501,
   };
   return (
     <div className="reviews app-width">
@@ -20,6 +21,9 @@ export default function Reviews({ amount }: ReviewsDataType) {
         .map((item) => (
           <Review reviewPrototype={reviewPrototype}></Review>
         ))}
+      <div className="reviews-all">See all reviews</div>
+      <h2 className="reviews-news">What's new</h2>
+      <h2 className="reviews-devs">Developer contact</h2>
     </div>
   );
 }
--- src/components/common/Review.tsx
+++ src/components/common/Review.tsx
@@ -8,6 +8,7 @@ interface ReviewsPrototypeDataType {
     score: string;
     scoreDate: string;
     review: string;
+    helpful: number;
   };
 }
 
@@ -27,11 +28,31 @@ export default function Review({ reviewPrototype }: ReviewsPrototypeDataType) {
           <img className="review-profile-button__img" src={points} alt=""></img>
         </button>
       </div>
-      <div>
+      <div className="review-score">
         <Stars width={12}></Stars>
-        <div>March 6, 2024</div>
+        <div className="review-score__date">{reviewPrototype.scoreDate}</div>
       </div>
-      <p>{reviewPrototype.review}</p>
+      <div className="review__text">{reviewPrototype.review}</div>
+      <div className="review__helpfull">
+        {reviewPrototype.helpful} people found this review helpful
+      </div>
+      <div className="review-check">
+        <div className="review-check__title">Did you find this helpful?</div>
+        <button className="review-check__btn">Yes</button>
+        <button className="review-check__btn">No</button>
+      </div>
+      <div className="review-answer">
+        <div className="review-answer-head">
+          <div className="review-answer-head__name">Casino</div>
+          <div className="review-answer-head__date">
+            {reviewPrototype.scoreDate}
+          </div>
+        </div>
+        <div className="review-answer__text">
+          We have taken your request into account and will forward it to the
+          technical department
+        </div>
+      </div>
     </div>
   );
 }
